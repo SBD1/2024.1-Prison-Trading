525b4a31cdfac077865744987085208ed6f44044
= <joaoseisei@gmail.com>
2024-08-17 16:55:48 -0300

    atualiza main

--- src/main.py
+++ src/main.py
@@ -45,124 +45,64 @@ class DatabaseConnection:
             self.connection.close()
 
 
-class Game:
-    def __init__(self, largura=900, altura=600, limiteFPS=100):
-        pygame.init()
-        self.largura = largura
-        self.altura = altura
-        self.limiteFPS = limiteFPS
-        self.ticks = 1000 / limiteFPS
-        self.tela = pygame.display.set_mode((largura, altura))
-        pygame.display.set_caption("Prison Trading")
-        self.isrunning = True
-        self.titulo = pygame.font.Font(None, 36)
-        self.subtitulo = pygame.font.Font(None, 20)
-        self.fps = 0
-        self.clock = pygame.time.Clock()
-        self.proximo_tick = pygame.time.get_ticks()
-        self.db = DatabaseConnection()
-
-        # ----- INFO DO JOGO ------- #
-        self.lugar_atualID = ""
-        self.lugar_atual = ""
-        self.descricao_lugar_atual = ""
-        self.lugares = []
-        self.itens = []
-        self.itens = ""
-
-    def update(self):
-        for evento in pygame.event.get():
-            if evento.type == pygame.QUIT:
-                self.isrunning = False
-
-    def render(self):
-        self.tela.fill((0, 0, 0))
-
-        fps_texto = self.titulo.render(f'FPS: {self.fps:.0f}', True, (255, 255, 255))
-        self.tela.blit(fps_texto, (10, 10))
-
-        lugar_texo = self.titulo.render(f'{self.lugar_atual}', True, (255, 255, 255))
-        descricao_lugar_texto = self.subtitulo.render(f'{self.descricao_lugar_atual}', True, (255, 255, 255))
-        self.tela.blit(lugar_texo, (10, 500))
-        self.tela.blit(descricao_lugar_texto, (10, 550))
-
-        y_offset = 150
-        for lugar, regiao in self.lugares:
-            lugar_texto = self.subtitulo.render(f'{lugar} - {regiao}', True, (255, 255, 255))
-            self.tela.blit(lugar_texto, (10, y_offset))
-            y_offset += 30
-
-        pygame.display.flip()
-
-    def main(self):
-        while self.isrunning:
-            self.update()
-            self.fps = self.clock.get_fps()
-            self.render()
-            self.proximo_tick += self.ticks
-            sleep_time = self.proximo_tick - pygame.time.get_ticks()
-            if sleep_time > 0:
-                time.sleep(sleep_time / 1000.0)
-            self.clock.tick(self.limiteFPS)
-        pygame.quit()
-
-    def atualizaItens(self):
-
-        self.db.cursor.execute("""
-            SELECT COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome) AS nome
-            FROM instancia_item ins
-            LEFT JOIN arma arm ON arm.id = ins.item
-            LEFT JOIN ferramenta fer ON fer.id = ins.item
-            LEFT JOIN comida com ON com.id = ins.item
-            LEFT JOIN medicamento med ON med.id = ins.item
-            LEFT JOIN utilizavel uti ON uti.id = ins.item
-            WHERE ins.lugar = %s
-            ORDER BY COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome);
-        """, (self.lugar_atualID,))
-        resultado = self.db.cursor.fetchall()
-
-        print(resultado)
-
-
-
-
-    def atualizarLugar(self):
-
-        self.db.cursor.execute("""
-            SELECT lug.nome, lug.descricao, jog.lugar
-            FROM jogador jog
-            LEFT JOIN lugar lug ON lug.id = jog.lugar
-            WHERE jog.id = 1;
-        """)
-
-        resultado = self.db.cursor.fetchone()
-        if resultado:
-            lugar_atual, descricao_lugar_atual, lugar_atual_id = resultado
-            self.lugar_atual = lugar_atual.strip()
-            self.descricao_lugar_atual = descricao_lugar_atual.strip()
-            self.lugar_atualID = lugar_atual_id
-
-            self.db.cursor.execute("""
-                SELECT lug.nome, reg.nome
-                FROM lugar_origem_destino ori
-                JOIN lugar lug ON ori.lugar_destino = lug.id
-                JOIN regiao reg ON lug.regiao = reg.id
-                WHERE ori.lugar_origem = %s
-                ORDER BY lug.nome;
-            """, (lugar_atual_id,))
-
-            resultado = self.db.cursor.fetchall()
-            self.lugares = [[lugar.strip(), regiao.strip()] for lugar, regiao in resultado]
-
-    def start(self):
-        self.db.connect()
-        self.atualizarLugar()
-        self.atualizaItens()
-        self.main()
-        self.db.close()
 
 
 
 if __name__ == "__main__":
-    prison_trading = Game()
-    prison_trading.start()
+    db = DatabaseConnection()
+    db.connect()
+
+    db.cursor.execute("""
+        SELECT jog.lugar
+        FROM jogador jog
+        WHERE jog.id = 1;
+    """)
+    results = db.cursor.fetchall()
+    lugar_atual = results[0][0]
+
+    print("Bem-vindo, digite "
+          "\n'ATUAL' - Para ver sua localização atual."
+          "\n'MAPA' - Para ver os possíveis lugares onde você pode ir."
+          "\n'MOVER + ' ' + ID' - Para se movimentar de um lugar para outro."
+          "\n'HELP' - Para ver os possíveis comandos.")
+
+    while True:
+        user_input = input()
+
+        if user_input == "ATUAL":
+            db.cursor.execute("""
+                SELECT lug.nome, lug.descricao, jog.lugar
+                FROM jogador jog
+                LEFT JOIN lugar lug ON lug.id = jog.lugar
+                WHERE jog.id = 1;
+            """)
+            resultado = db.cursor.fetchall()
+            print(resultado)
+
+        elif user_input == "MAPA":
+            db.cursor.execute("""
+               SELECT lug.nome, reg.nome
+               FROM lugar_origem_destino ori
+               JOIN lugar lug ON ori.lugar_destino = lug.id
+               JOIN regiao reg ON lug.regiao = reg.id
+               WHERE ori.lugar_origem = %s
+               ORDER BY lug.nome;
+            """, (lugar_atual,))
+            resultado = db.cursor.fetchall()
+            headers = ["Lugar", "Região"]
+            print(tabulate(resultado, headers=headers, tablefmt="grid"))
+
+        elif user_input.startswith("MOVER "):
+
+            _, lugar_id = user_input.split(maxsplit=1)
+            print(f"Movendo para o lugar com ID: {lugar_id}")
+
+        elif user_input == "HELP":
+            print("Bem-vindo, digite "
+                  "\n'MAPA' - Para ver os possíveis lugares onde você pode ir."
+                  "\n'MOVER + ' ' + ID' - Para se movimentar de um lugar para outro."
+                  "\n'HELP' - Para ver os possíveis comandos.")
+        else:
+            print("Comando desconhecido. Digite 'HELP' para ver os possíveis comandos.")
+
+    db.close()
