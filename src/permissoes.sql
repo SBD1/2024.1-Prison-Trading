530e68e5eb2461c602b1679759f629ef58dd933b
João G. Carvalho <109870486+joaoseisei@users.noreply.github.com>
2024-08-30 09:34:31 -0300

    Update permissoes.sql

--- src/permissoes.sql
+++ src/permissoes.sql
@@ -458,4 +458,163 @@ CREATE TRIGGER update_inventario
 BEFORE update ON inventario
 FOR EACH ROW EXECUTE PROCEDURE update_inventario();
 
+---------------------
+---
+---   INSTANCIA_ITEM
+---
+---------------------
+
+CREATE FUNCTION insert_instancia()
+RETURNS trigger AS $insert_instancia$
+BEGIN
+    IF NEW.lugar IS NULL AND NEW.inventario IS NULL THEN
+        RAISE EXCEPTION 'Atualizando inventário e lugar como valores nulos.';
+    ELSIF NEW.lugar IS NOT NULL AND NEW.inventario IS NOT NULL THEN
+        RAISE EXCEPTION 'A instância não pode estar em um lugar e inventário ao mesmo tempo.';
+    END IF;
+
+    RETURN NEW;
+END;
+$insert_instancia$ LANGUAGE plpgsql;
+
+CREATE TRIGGER insert_instancia
+BEFORE INSERT ON instancia_item
+FOR EACH ROW EXECUTE PROCEDURE insert_instancia();
+
+CREATE FUNCTION update_instancia()
+RETURNS trigger AS $update_instancia$
+DECLARE
+    inventario_atb INTEGER;
+    item_atb INTEGER;
+    inventario_ocupado_atb SMALLINT;
+    tamanho_inventario_atb INTEGER;
+    tamanho_item_atb INTEGER;
+    total_atb SMALLINT;
+    lugar_jogador_atb INTEGER;
+
+BEGIN
+    IF NEW.lugar IS NULL AND NEW.inventario IS NULL THEN
+        RAISE EXCEPTION 'Atualizando inventário e lugar como valores nulos.';
+    ELSIF NEW.lugar IS NOT NULL AND NEW.inventario IS NOT NULL THEN
+        RAISE EXCEPTION 'A instância não pode estar em um lugar e inventário ao mesmo tempo.';
+    END IF;
+
+    inventario_atb := NEW.inventario;
+    item_atb := NEW.item;
+
+    SELECT inventario_ocupado, tamanho INTO inventario_ocupado_atb, tamanho_inventario_atb
+    FROM inventario
+    WHERE id = inventario_atb;
+
+    SELECT COALESCE(arm.tamanho, fer.tamanho, com.tamanho, med.tamanho, uti.tamanho) INTO tamanho_item_atb
+    FROM item ite
+    LEFT JOIN arma arm ON arm.id = ite.id
+    LEFT JOIN ferramenta fer ON fer.id = ite.id
+    LEFT JOIN comida com ON com.id = ite.id
+    LEFT JOIN medicamento med ON med.id = ite.id
+    LEFT JOIN utilizavel uti ON uti.id = ite.id
+    WHERE ite.id = item_atb;
+
+    IF OLD.lugar IS DISTINCT FROM NEW.lugar THEN
+
+        IF NEW.pessoa IS NOT NULL THEN
+            SELECT lugar INTO lugar_jogador_atb
+            FROM jogador
+            WHERE id = NEW.pessoa;
+            IF lugar_jogador_atb <> OLD.lugar THEN
+                RAISE EXCEPTION 'Jogador não pode pegar o item, pois não está no mesmo lugar da instância.';
+            END IF;
+        ELSE
+            SELECT lugar INTO lugar_jogador_atb
+            FROM jogador
+            WHERE id = OLD.pessoa;
+            IF lugar_jogador_atb <> NEW.lugar THEN
+                RAISE EXCEPTION 'Jogador não pode dropar o item, pois não está no mesmo lugar de onde vai dropar.';
+            END IF;
+        END IF;
+
+    END IF;
+
+    IF NEW.inventario IS NULL THEN
+        SELECT inventario_ocupado INTO inventario_ocupado_atb
+        FROM inventario
+        WHERE id = OLD.inventario;
+
+        total_atb := inventario_ocupado_atb - tamanho_item_atb;
+
+        RAISE NOTICE 'Joguou Item fora - inventário ocupado: %', total_atb;
+
+        UPDATE inventario
+        SET inventario_ocupado = total_atb
+        WHERE id = OLD.inventario;
+
+    ELSIF NEW.inventario IS NOT NULL THEN
+
+        total_atb := inventario_ocupado_atb + tamanho_item_atb;
+
+        IF total_atb > tamanho_inventario_atb THEN
+         	RAISE EXCEPTION 'Espaço insuficiente no inventário. Atualização bloqueada. Total ocupado: % | Tamanho do item: % | Total atual: %',
+          	inventario_ocupado_atb, tamanho_item_atb, total_atb;
+        END IF;
+
+        RAISE NOTICE 'Pegou Item - inventário ocupado: %', total_atb;
+
+        UPDATE inventario
+        SET inventario_ocupado = total_atb
+        WHERE id = inventario_atb;
+
+    END IF;
+
+    RETURN NEW;
+
+END;
+$update_instancia$ LANGUAGE plpgsql;
+
+CREATE TRIGGER update_instancia
+BEFORE UPDATE ON instancia_item
+FOR EACH ROW EXECUTE PROCEDURE update_instancia();
+
+CREATE FUNCTION delete_instancia()
+RETURNS trigger AS $delete_instancia$
+DECLARE
+    item_atb INTEGER;
+    inventario_ocupado_atb SMALLINT;
+    tamanho_item_atb INTEGER;
+    total_atb SMALLINT;
+BEGIN
+
+    IF OLD.inventario IS NOT NULL THEN
+        item_atb := OLD.item;
+
+        SELECT COALESCE(arm.tamanho, fer.tamanho, com.tamanho, med.tamanho, uti.tamanho) INTO tamanho_item_atb
+        FROM item ite
+        LEFT JOIN arma arm ON arm.id = ite.id
+        LEFT JOIN ferramenta fer ON fer.id = ite.id
+        LEFT JOIN comida com ON com.id = ite.id
+        LEFT JOIN medicamento med ON med.id = ite.id
+        LEFT JOIN utilizavel uti ON uti.id = ite.id
+        WHERE ite.id = item_atb;
+
+        SELECT inventario_ocupado INTO inventario_ocupado_atb
+        FROM inventario
+        WHERE id = OLD.inventario;
+
+        total_atb := inventario_ocupado_atb - tamanho_item_atb;
+
+        RAISE NOTICE 'Joguou Item fora - inventário ocupado: %', total_atb;
+
+        UPDATE inventario
+        SET inventario_ocupado = total_atb
+        WHERE id = OLD.inventario;
+
+    END IF;
+
+    RETURN OLD;
+END;
+$delete_instancia$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_instancia
+BEFORE DELETE ON instancia_item
+FOR EACH ROW EXECUTE PROCEDURE delete_instancia();
+
 COMMIT;
