2554bf7afb71d8318795113a8a2694499f137d6d
= <joaoseisei@gmail.com>
2024-09-03 20:22:50 -0300

    adiciona views

--- src/main.py
+++ src/main.py
@@ -93,7 +93,6 @@ class Game:
 
     def help(self):
         print("Bem-vindo, digite "
-              "\n'INVENTARIO' - Para ver os itens que estão no seu inventário."
               "\n'INFO' - Para ver as informações de um item."
               "\n'MOVER + ' ' + ID' - Para se movimentar de um lugar para outro."
               "\n'PEGAR + ' ' + ID' - Para pegar um item no chão."
@@ -102,59 +101,28 @@ class Game:
               "\n'CLEAR' - Para limpar o terminal."
               "\n'SAIR' - Para fechar o jogo.")
 
-    def atual(self):
-        query = db.execute_fetchone("""
-            SELECT lug.id, lug.nome, lug.descricao
-            FROM jogador jog
-            LEFT JOIN lugar lug ON lug.id = jog.lugar
-            WHERE jog.id = %s;
-        """, (self.id_jogador,))
+    def lugar_at(self):
+        query = db.execute_fetchone("SELECT id, nome, descricao FROM detalhes_lugar WHERE id_jogador = %s;", (self.id_jogador,))
+        if query:
+            return query
+
+    def regiao_at(self):
+        query = db.execute_fetchone("SELECT * FROM detalhes_regiao WHERE id_jogador = %s;", (self.id_jogador,))
         if query:
             return query
 
     def mapa(self):
-        query = db.execute_fetchall("""
-            SELECT lug.id, lug.nome
-            FROM lugar_origem_destino ori
-            JOIN lugar lug ON ori.lugar_destino = lug.id
-            JOIN regiao reg ON lug.regiao = reg.id
-            WHERE ori.lugar_origem = %s
-            ORDER BY lug.nome;
-        """, (self.lugar_atual,))
+        query = db.execute_fetchall("SELECT * FROM lugares_ori_des_detalhado WHERE lugar_origem = %s;", (self.lugar_atual,))
         if query:
             return query
 
     def pessoas(self):
-        query = db.execute_fetchall("""
-            WITH Pessoas AS (
-                SELECT id, nome, lugar FROM jogador
-                UNION ALL
-                SELECT id, nome, lugar FROM policial
-                UNION ALL
-                SELECT id, nome, lugar FROM prisioneiro
-                UNION ALL
-                SELECT id, nome, lugar FROM informante
-            )
-            SELECT pes.nome, tip.tipo, pes.id
-            FROM pessoas pes, pessoa tip
-            WHERE pes.id = tip.id AND pes.lugar = %s
-            ORDER BY tip.tipo, pes.id;
-        """, (self.lugar_atual,))
+        query = db.execute_fetchall("SELECT * FROM pessoas_lugar WHERE lugar = %s;", (self.lugar_atual,))
         if query:
             return query
 
     def itens(self):
-        query = db.execute_fetchall("""
-            SELECT ins.id, COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome) AS nome
-            FROM instancia_item ins
-            LEFT JOIN arma arm ON arm.id = ins.item
-            LEFT JOIN ferramenta fer ON fer.id = ins.item
-            LEFT JOIN comida com ON com.id = ins.item
-            LEFT JOIN medicamento med ON med.id = ins.item
-            LEFT JOIN utilizavel uti ON uti.id = ins.item
-            WHERE ins.lugar = %s
-            ORDER BY nome;
-        """, (self.lugar_atual,))
+        query = db.execute_fetchall("SELECT * FROM instancias_detalhadas WHERE lugar = %s;", (self.lugar_atual,))
         if query:
             return query
 
@@ -182,82 +150,25 @@ class Game:
                 self.clear()
                 return opcao
 
-    def inventario(self):
-        query = db.execute_fetchall("""
-            SELECT COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome) AS nome, i.inventario_ocupado, i.tamanho
-            FROM inventario AS i
-            LEFT JOIN instancia_item AS t ON i.id = t.inventario
-            LEFT JOIN arma AS arm ON arm.id = t.item
-            LEFT JOIN ferramenta AS fer ON fer.id = t.item
-            LEFT JOIN comida AS com ON com.id = t.item
-            LEFT JOIN medicamento AS med ON med.id = t.item
-            LEFT JOIN utilizavel AS uti ON uti.id = t.item
-            WHERE i.pessoa = %s
-            ORDER BY nome;
-        """, (self.id_jogador,))
-
-        if query:
-            print("Inventário:")
-            print("Tamanho do Inventário: " + str(query[0][2]) + "\tTamanho Ocupado: " + str(query[0][1]))
-            for resultado in query:
-                if query[0][0] is not None:
-                    print(resultado[0])
-        if any(resultado[0] is None for resultado in query):
-            print("Nenhum item no inventário.")
-
     def info(self):
-        nomeItem = str(input("Digite nome do item: "))
-
-        query = db.execute_fetchall("""
-            SELECT COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome) AS nome,
-                   COALESCE(arm.tamanho, fer.tamanho, com.tamanho, med.tamanho, uti.tamanho) AS tamanho,
-                   COALESCE(arm.descricao, fer.descricao, com.descricao, med.descricao, uti.descricao) AS descricao,
-                   arm.dano AS dano,
-                   fer.utilidade AS utilidade,
-                   COALESCE(com.raridade, med.raridade, uti.raridade) AS raridade,
-                   COALESCE(com.quantidade, med.quantidade, uti.quantidade) AS quantidade,
-                   com.recuperacao_vida AS recuperacao_vida,
-                   med.cura AS cura,
-                   uti.durabilidade AS durabilidade,
-                   CASE
-                        WHEN arm.nome IS NOT NULL THEN 'arma'
-                        WHEN fer.nome IS NOT NULL THEN 'ferramenta'
-                        WHEN com.nome IS NOT NULL THEN 'comida'
-                        WHEN med.nome IS NOT NULL THEN 'medicamento'
-                        WHEN uti.nome IS NOT NULL THEN 'utilizavel'
-                   ELSE 'desconhecido'
-               END AS tipo
-            FROM inventario AS i
-            LEFT JOIN instancia_item AS t ON i.id = t.inventario
-            LEFT JOIN arma AS arm ON arm.id = t.item
-            LEFT JOIN ferramenta AS fer ON fer.id = t.item
-            LEFT JOIN comida AS com ON com.id = t.item
-            LEFT JOIN medicamento AS med ON med.id = t.item
-            LEFT JOIN utilizavel AS uti ON uti.id = t.item
-            WHERE i.pessoa = %s AND COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome) = %s
-        """, (self.id_jogador, nomeItem,))
-
+        nomeItem = str(input("\033[93mDigite nome do item: \033[0m"))
+        query = db.execute_fetchall("SELECT * FROM info_item WHERE nome = %s;", (nomeItem,))
         if query:
             for resultado in query:
                 if resultado[0] is not None:
-                    if resultado[10] == "arma":
-                        print(resultado[0] + "\nTamanho: " + str(resultado[1]) + "\n" + resultado[2] + "\nDano: " + str(
-                            resultado[3]))
-                    if resultado[10] == "ferramenta":
-                        print(resultado[0] + "\nTamanho: " + str(resultado[1]) + "\n" + resultado[
-                            2] + "\nUtilidade: " + str(resultado[4]))
-                    if resultado[10] == "comida":
-                        print(resultado[0] + "\nTamanho: " + str(resultado[1]) + "\n" + resultado[
-                            2] + "\nRaridade: " + str(resultado[5]) + "\nQuantidade: " + str(
-                            resultado[6]) + "\nRecuperacao de Vida: " + str(resultado[7]))
-                    if resultado[10] == "medicamento":
-                        print(resultado[0] + "\nTamanho: " + str(resultado[1]) + "\n" + resultado[
-                            2] + "\nRaridade: " + str(resultado[5]) + "\nQuantidade: " + str(
-                            resultado[6]) + "\nCura: " + str(resultado[8]))
-                    if resultado[10] == "utilizavel":
-                        print(resultado[0] + "\nTamanho: " + str(resultado[1]) + "\n" + resultado[
-                            2] + "\nRaridade: " + str(resultado[5]) + "\nQuantidade: " + str(
-                            resultado[6]) + "\nDurabilidade: " + str(resultado[9]))
+                    tipo = resultado[10]
+                    detalhes = f"{resultado[0]}\nTamanho: {resultado[1]}\n{resultado[2]}"
+                    if tipo == "arma":
+                        detalhes += f"\nDano: {resultado[3]}"
+                    elif tipo == "ferramenta":
+                        detalhes += f"\nUtilidade: {resultado[4]}"
+                    elif tipo == "comida":
+                        detalhes += f"\nRaridade: {resultado[5]}\nQuantidade: {resultado[6]}\nRecuperação de Vida: {resultado[7]}"
+                    elif tipo == "medicamento":
+                        detalhes += f"\nRaridade: {resultado[5]}\nQuantidade: {resultado[6]}\nCura: {resultado[8]}"
+                    elif tipo == "utilizavel":
+                        detalhes += f"\nRaridade: {resultado[5]}\nQuantidade: {resultado[6]}\nDurabilidade: {resultado[9]}"
+                    print(detalhes)
         else:
             print("Nenhuma informação para ver.")
 
@@ -267,25 +178,17 @@ class Game:
 
         if opcao == 0:
             print("\033[92mÁREA DE LOGIN\n\033[0m")
-            query = db.execute_fetchall("""
-                SELECT jog.id, jog.nome, jog.nivel
-                FROM jogador jog
-                ORDER BY jog.id;
-            """)
+            query = db.execute_fetchall("SELECT jog.id, jog.nome, jog.nivel FROM jogador jog ORDER BY jog.id;")
             if query:
-                print("+----------------+---------------+--------------------------------------------------------------------+")
+                linha = "+----------------+---------------+--------------------------------------------------------------------+"
+                print(linha)
                 for resultado in query:
                     print(f'| ID: {resultado[0]:02}\t | Nível: {resultado[2]:02}\t | Nome: {resultado[1]} |')
-                print("+----------------+---------------+--------------------------------------------------------------------+")
+                print(linha)
                 self.id_jogador = input("\033[92mDigite o ID do jogador escolhido para selecioná-lo: \033[0m")
-                query = db.execute_fetchone("""
-                    SELECT jog.nome, jog.lugar, jog.regiao
-                    FROM jogador jog
-                    WHERE jog.id = %s;
-                """, (self.id_jogador,))
+                query = db.execute_fetchone("SELECT nome FROM jogador WHERE id = %s;", (self.id_jogador,))
                 if query:
-                    nome, self.lugar_atual, self.regiao_atual = query
-                    print(f'\033[92mLogin realizado com sucesso. \nBem-vindo {nome}\033[0m')
+                    print(f'\033[92mLogin realizado com sucesso. \nBem-vindo {query[0]}\033[0m')
                     time.sleep(3)
                     self.clear()
                     return
@@ -297,20 +200,8 @@ class Game:
             print("\033[92mÁREA DE REGISTRO\n\033[0m")
             nome = input("Digite o nome do jogador: ")
             db.execute_commit("""
-                BEGIN;
-                INSERT INTO pessoa (tipo)
-                VALUES ('jogador')
-                RETURNING id;
-
-                DO $$ 
-                DECLARE 
-                    novo_id INT;
-                BEGIN
-                    SELECT MAX(id) INTO novo_id FROM pessoa;
-                    INSERT INTO jogador (id, nome, habilidade_briga, vida, forca, tempo_vida, gangue, nivel, missao, lugar, regiao)
-                    VALUES (novo_id, %s, 2, 5, 3, 10, NULL, 0, NULL, 2, 1);
-                END $$;
-                COMMIT;
+                INSERT INTO jogador (id, nome, habilidade_briga, vida, forca, tempo_vida, gangue, nivel, missao, lugar, regiao)
+                VALUES (1, %s, 2, 5, 3, 10, NULL, 0, NULL, 2, 1);
             """, (nome,))
             print("\033[92mJogador salvo com sucesso.\033[0m")
             time.sleep(3)
@@ -338,16 +229,33 @@ class Game:
     def move_cursor_to(self, x, y):
         print(f"\033[{y};{x}H", end='')
 
+    def print_section(header, items, line_offset, color="\033[93m"):
+        self.move_cursor_to(115, linha_atual)
+        print(header)
+        linha_atual += 2
+        if items:
+            for resultado in items:
+                self.move_cursor_to(115, linha_atual)
+                if color == "\033[96m" and resultado[1] == "policial":
+                    color = "\033[96m"
+                else:
+                    color = "\033[93m"
+                print(f'{color}{resultado[2]:02} - {resultado[0].strip()}\033[0m')
+                linha_atual += 1
+        else:
+            self.move_cursor_to(115, linha_atual)
+            print(f'Nenhum item nesse lugar.')
+            linha_atual += 1
+        linha_atual += 1
+        self.move_cursor_to(114, linha_atual)
+        print("\033[91m======================================================\033[0m")
+        return linha_atual
+
     def status_jogador(self):
-        query = db.execute_fetchone("""
-            SELECT j.nome, j.habilidade_briga, j.vida, j.forca, j.tempo_vida, j.gangue, j.nivel, j.missao, j.lugar, j.regiao, i.tamanho, i.inventario_ocupado 
-            FROM jogador j
-            JOIN inventario i ON i.pessoa = j.id
-            WHERE j.id = %s;
-        """, (self.id_jogador,))
+        query = db.execute_fetchone("SELECT * FROM status_jogador WHERE id = %s;", (self.id_jogador,))
         if query:
-            self.nome, self.habilidade_briga, self.vida, self.forca, self.tempo_vida, self.gangue, self.nivel, self.missao, self.lugar_atual, self.regiao_atual, tamanho, self.inventario_ocupado= query
-            id, nome, descricao = self.atual()
+            id, self.nome, self.habilidade_briga, self.vida, self.forca, self.tempo_vida, self.gangue, self.nivel, self.missao, self.lugar_atual, self.regiao_atual, tamanho, self.inventario_ocupado= query
+            id, nome, descricao = self.lugar_at()
 
             print("\033[93m" + "+-" * (110 // 2) + "+")
             print(f'ID: {self.id_jogador}\t\t\tNível: {self.nivel} \t\tNome: {self.nome}')
@@ -390,7 +298,7 @@ class Game:
             if itens:
                 for resultado in itens:
                     self.move_cursor_to(115, linha_atual)
-                    print(f'{resultado[0]:02} - {resultado[1].strip()}')
+                    print(f'{resultado[0]:02} - {resultado[2]:02} - {resultado[1].strip()}')
                     linha_atual += 1
             else:
                 self.move_cursor_to(115, linha_atual)
@@ -413,18 +321,7 @@ class Game:
             self.move_cursor_to(114, linha_atual)
             print("\033[91m======================================================\033[0m")
 
-            query = db.execute_fetchall("""
-                SELECT t.id, COALESCE(arm.nome, fer.nome, com.nome, med.nome, uti.nome) AS nome
-                FROM inventario AS i
-                LEFT JOIN instancia_item AS t ON i.id = t.inventario
-                LEFT JOIN arma AS arm ON arm.id = t.item
-                LEFT JOIN ferramenta AS fer ON fer.id = t.item
-                LEFT JOIN comida AS com ON com.id = t.item
-                LEFT JOIN medicamento AS med ON med.id = t.item
-                LEFT JOIN utilizavel AS uti ON uti.id = t.item
-                WHERE i.pessoa = %s
-                ORDER BY nome;
-            """, (self.id_jogador,))
+            query = db.execute_fetchall("SELECT * FROM itens_inventario WHERE pessoa = %s;", (self.id_jogador,))
             linha_atual += 1
             self.move_cursor_to(115, linha_atual)
             print("\t\tINVENTÁRIO:")
@@ -446,72 +343,38 @@ class Game:
 
     def mover(self, input_usuario):
         _, lugar_id = input_usuario.split(maxsplit=1)
-
-        query = db.execute_fetchall("""
-            SELECT lug.id
-            FROM lugar_origem_destino ori
-            JOIN lugar lug ON ori.lugar_destino = lug.id
-            JOIN regiao reg ON lug.regiao = reg.id
-            WHERE ori.lugar_origem = %s
-            ORDER BY lug.nome;
-        """, (self.lugar_atual,))
-
-        verifica = any(str(resultado[0]) == lugar_id for resultado in query)
-
-        if verifica:
-            query = db.execute_fetchall("""
-               SELECT regiao 
-               FROM lugar
-               WHERE id = %s;
-            """, (lugar_id,))
-            regiao = query[0][0]
-
-            db.execute_commit("""
-                UPDATE Jogador 
-                SET lugar = %s, regiao = %s
-                WHERE id = 1;
-            """, (lugar_id, regiao))
-
-            self.lugar_atual = lugar_id
-            self.regiao_atual = regiao
-            print("Posição movida com sucesso.")
-        else:
-            print("A sala atual não possui conexões com a que o jogador digitou.")
+        db.execute_commit("SELECT movimenta_jogador(%s, %s);", (self.id_jogador, lugar_id))
 
     def pegar(self, input_usuario):
         _, id_inst = input_usuario.split(maxsplit=1)
-        db.execute_commit("""
-            SELECT pegar_item_chao(%s, %s);
-        """, (self.id_jogador, id_inst))
+        db.execute_commit("SELECT pegar_item_chao(%s, %s);", (self.id_jogador, id_inst))
 
     def largar(self, input_usuario):
         _, id_inst = input_usuario.split(maxsplit=1)
-        db.execute_commit("""
-            SELECT dropar_item_chao(%s, %s);
-        """, (self.id_jogador, id_inst))
+        db.execute_commit("SELECT dropar_item_chao(%s, %s);", (self.id_jogador, id_inst))
 
     def livro(self):
-        # TODO ADICIONAR AS VIEWS DE CRAFT
-        query = db.execute_fetchall("""
-           SELECT id, nome 
-           FROM livro_fabricacao
-        """, )
+        query = db.execute_fetchall("SELECT id, nome FROM livro_fabricacao", )
         if query:
             for resultado in query:
                 print(f'ID: {resultado[0]}\tNome: {resultado[1]}')
 
-        tipo_livro = input("Digite o id do livro: ")
+        tipo_livro = input("\033[93mDigite o id do livro: \033[0m")
+        query = db.execute_fetchall("SELECT * FROM itens_livro_fabricacao WHERE livro_fabricacao = %s;", (tipo_livro,))
+        if query:
+            for resultado in query:
+                print(f'ID: {resultado[1]}\tNome: {resultado[2]}')
 
-        query = db.execute_fetchall("""
-           SELECT id, nome 
-           FROM livro_fabricacao
-        """, )
+        id_item = input("\033[93mDigite o id do item que deseja visualizar a receita de fabricação: \033[0m")
+        query = db.execute_fetchall("SELECT * FROM craft_item WHERE item_fabricavel = %s;", (id_item,))
+        if query:
+            print("\033[92m\nItens necessários:\033[93m")
+            for resultado in query:
+                print(f'\033[92m • {resultado[1]}\033[93m')
 
-        id_item = input("Digite o id do item que deseja fabricar: ")
-
-        db.execute_commit("""
-            SELECT realizar_craft(%s, %s);
-        """, (self.id_jogador, id_item))
+    def craft(self, input_usuario):
+        _, id_item = input_usuario.split(maxsplit=1)
+        db.execute_commit("SELECT realizar_craft(%s, %s);",(self.id_jogador, id_item))
 
     def clear(self):
         os.system('cls' if os.name == 'nt' else 'clear')
@@ -523,7 +386,6 @@ class Game:
         comandos = {
             "HELP": self.help,
             "CLEAR": self.clear,
-            "INVENTARIO": self.inventario,
             "INFO": self.info,
             "LIVRO": self.livro,
         }
@@ -540,6 +402,10 @@ class Game:
                 self.clear()
             elif input_usuario.startswith("LARGAR "):
                 self.largar(input_usuario)
+                input("\n\033[93mPrecione qualquer tecla atualizar\033[0m")
+                self.clear()
+            elif input_usuario.startswith("CRAFT "):
+                self.craft(input_usuario)
                 input("\n\033[93mPrecione qualquer tecla atualizar\033[0m")
                 self.clear()
             elif input_usuario == "SAIR":
